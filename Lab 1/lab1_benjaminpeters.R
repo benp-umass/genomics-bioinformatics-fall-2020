878126fbf2aed5ed4ff38de03ab02531a4bf2fad
Ben Peters <benjaminpete@umass.edu>
2020-08-31 10:33:10 -0400

    Add lab 1 R, Rmd, and html files to repo

---
+++ Lab 1/lab1_benjaminpeters.R
@@ -0,0 +1,30 @@
+##Lab 1 Exercises
+##Benjamin Peters
+
+
+##Exercise 1
+x <- 3
+y <- 15
+
+x+y
+y-x
+x*y
+y/x
+
+
+##Exercise 2
+x <- 2
+y <- 3
+z <- (x^5) > (y^4)
+z
+
+
+##Exercise 3
+x <- c(22, 62, 148, 43, 129)
+sum(x)
+x/(sum(x))
+
+
+##Exercise 4
+DNA <- c("A","T","C","G")
+sort(DNA)
